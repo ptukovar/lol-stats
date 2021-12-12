5b880e328e9a39c66bcd415ad64b836e78d848b2
ptukovar <53978671+ptukovar@users.noreply.github.com>
2021-12-12 09:16:50 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,11 +2,14 @@
 
 int main(int argc, char *argv[])
 {
-    if(argc!=4){
+    if (argc != 4)
+    {
         fprintf(stderr, "Error: Missing command line arguments!\n");
         fprintf(stderr, "Use:   ./main input_file_with_matches.txt input_file_with_nicknames.txt output_file.html\n");
         exit(EXIT_FAILURE);
-    }else{
+    }
+    else
+    {
         Player p[max_players];
         char *fnameH = argv[1];
         char *fnameN = argv[2];
@@ -14,8 +17,8 @@ int main(int argc, char *argv[])
         consstr(p);
         read_matchF(fnameH, p);
         read_nicksF(fnameN, p);
-        print_stats(fnameO,p);
+        print_stats(fnameO, p);
     }
-    
+
     return 0;
 }
